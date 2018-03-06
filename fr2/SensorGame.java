1d3ec7ac041e30b67f6811d1188fc0bffb00a7d2
takagotch <dyaccb@gmail.com>
2018-03-07 03:16:45 +0900

    getWindow(); requestWindowFeature(); Window.FEATURE_NO_TITLE layout.addView(); sensorView

--- fr2/SensorGame.java
+++ fr2/SensorGame.java
@@ -3,9 +3,15 @@ public void onCreate(Bundle bundle){
   super.onCreate(bundle);
   Util.setActivity(this);
 
-  getWindow().clearFlags();
+  getWindow().clearFlags(
+	WindowManager.LayoutParams.FLAG_FORCE_FULLSCREEN);
+  getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+  requestWindowFeature(Window.FEATURE_NO_TITLE);
 
   RelativeLayout layout = new RelativeLayout(this);
+  setContentView(layout);
+  sensorView = new SensorView(this);
+  layout.addView(sensorView);
 
   Point displaySize = Util.getDisplaySize();
   int dstW = displaySize.x;
@@ -15,5 +21,19 @@ public void onCreate(Bundle bundle){
   params.setMargins(0, (sidplaySize.y-dstH)/2,
 	0, (displaySize.y-dstH)/2);
   sensorView.setLayoutParams(params);
+
+  //...
 }
+
+
+public final static int
+  S_TITLE = 0,
+  S_PLAY = 1,
+  S_GOAL = 2,
+  S_GAMEOVER = 3;
+
+private int scene;
+
+
+
 
