bb55a62a0ce289b406f0b862986578a6248d5cba
takagotch <dyaccb@gmail.com>
2018-03-07 03:13:17 +0900

    getWindow(); requestWindowFeature(); Window.FEATURE_NO_TITLE layout.addView(); sensorView

--- fr2/SensorGame.java
+++ fr2/SensorGame.java
@@ -8,6 +8,12 @@ public void onCreate(Bundle bundle){
   RelativeLayout layout = new RelativeLayout(this);
 
   Point displaySize = Util.getDisplaySize();
-  
+  int dstW = displaySize.x;
+  int dstH = displaySize.x*SensorView.H/SensorView.W;
+  RelativeLayout.LayoutParams params;
+  params = new RelativeLayout.LayoutParams(dstW, dstH);
+  params.setMargins(0, (sidplaySize.y-dstH)/2,
+	0, (displaySize.y-dstH)/2);
+  sensorView.setLayoutParams(params);
 }
 
