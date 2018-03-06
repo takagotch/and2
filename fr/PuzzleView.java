74cf6a55a7c31b51afd76500d306eef0796b4e30
takagotch <dyaccb@gmail.com>
2018-03-06 18:49:45 +0900

    public class PuzzleView extends View{} private int scene = S_TITLE; private int[] data = new int[25];

---
+++ fr/PuzzleView.java
@@ -0,0 +1,84 @@
+package net.tky.puzzlegame;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class PuzzleView extends View {
+  private final static int
+    S_TITLE = 0,
+    S_PLAY = 1,
+    S_CLEAR = 2;
+
+  public final static int
+	  W = 750,
+	  H = 1334;
+
+  private int scene = S_TITLE;
+  private int[] data = new int[25];
+  private int shuffle;
+
+  private Graphics g;
+  private Rect gSrc;
+  private Rect gDst;
+
+  private Bitmap imgBg;
+  private Bitmap imgFrame;
+  private Bitmap imgPic;
+  private Bitmap imgTitle;
+  private Bitmap imgTap;
+  private Bitmap imgClear;
+
+  public PuzzleView(Activity activity){
+    super(activity);
+
+    imgBg = Util.resbmp();
+
+    Point displaySize = Util.getDisplaySize();
+
+    g = new Graphics(W, H);
+
+    initScene(S_TITLE);
+  }
+
+  private void initScene(int scene){
+    this.scene = scene;
+
+    if(){
+    
+    }
+
+    else if(){
+    
+    
+    }
+    invalidate();
+  }
+
+  @Override
+  protected void onDraw(){
+  
+  }
+
+  if(){
+  
+  }
+
+  else fi (){
+  
+  }
+
+  canvas.drawColor();
+  canvas.drawBitmap();
+}
+
+@Override
+public boolean onTouchEvent(){
+
+}
+
