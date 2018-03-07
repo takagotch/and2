c691867e35655b2d17ee9447807f9f7da096674c
takagotch <dyaccb@gmail.com>
2018-03-07 22:42:36 +0900

    pictureSize = getPictureSize(cameraInfo);

--- fr3/CameraView.java
+++ fr3/CameraView.java
@@ -23,4 +23,22 @@ public CameraView(Context context){
   });
 }
 
+previewSize = getPreviewSize(cameraInfo);
+
+
+private void updateLayoutParams(){
+  int srcW = Math.min(
+       previewSize.getWidth(),
+       previewSize.getHeight());
+  int srcH = Math.max(
+       previewSize.getWidth(),
+       previewSize.getHeight());
+
+  int dstW = viewW;
+  int dstH = viewW*srcH/srcW;
+  RelativeLayout.LayoutParams params;
+  params = new RelativeLayout.LayoutParams(dstW, dstH);
+  params.setMargins(0, (viewH-dstH)/2, 0, 0);
+  setLayoutParams(params);
+}
 
