49289d6de705d5b21fce711b827e2b5bb960e450
takagotch <dyaccb@gmail.com>
2018-03-09 00:11:29 +0900

    android:duration=@android:integer/config_mediumAnimTime

---
+++ sound/LaunchAnimationEx.java
@@ -0,0 +1,12 @@
+package net.tky.launchanimationx;
+import android.app.Activity;
+import android.app.Acitivty;
+
+public class LaunchAnimationEx extends Acitivity implements
+ View.OnClickListener{
+ private final static int WC = ViewGroup.LayoutParams.WRAP_CONTENT;
+
+
+ }
+
+
