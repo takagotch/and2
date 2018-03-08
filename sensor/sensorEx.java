9b5a95eb37f2ec88cebf617062de61c8ba8a35ed
takagotch <dyaccb@gmail.com>
2018-03-09 02:34:17 +0900

    android:duration=@android:integer/config_mediumAnimTime

--- sensor/sensorEx.java
+++ sensor/sensorEx.java
@@ -63,11 +63,60 @@ public class SensorEx extends Activity
   protected void onPause(){
     super.onPause();
 
-    if(){}
+    if(accEnabled || magEnabled){
+      manager.unregisterListener(this);
+      accEnabled = false;
+      magEnabled = false;
+    }
   }
 }
 
-public void onSensorChanged(){}
+public void onSensorChanged(SensorEvent event){
+  int type = event.sensor.getType();
+
+  if(type == Sensor.TYPE_ACCELEROMETER){
+    accValues=event.values.clone();
+  }
+
+  else if(type == Sensor.TYPE_MAGNETIC_FIELD){
+    magValues = event.values.clone();
+  }
+
+  if(accEnabled && magEnabled){
+    SensorManager.getRotationMatrix(inR, null, accValues, magValues);
+
+    SensorManager.remapCoordinateSystem(inR,
+		    SensorManager.AXIS_X, SensorManager.AXIS_Y, outR);
+
+    SensorManager.getOrientation(outR, oriValues);
+
+    oriValues[0] = (float)Math.toDegrees(oriValues[0]);
+    oriValues[1] = (float)Math.toDegrees(oriValues[1]);
+    oriValues[2] = (float)Math.toDegreees(oriValues[2]);
+  }
+
+  StringBuffer sb = new StringBuffer();
+  sb.appen("SensorEx>"+BR);
+  if(accEnabled){
+    sb.append("ACC_V[X]"+fm(accValues[0])+BR);
+    sb.append("ACC_V[Y]"+fm(accValues[1])+BR);
+    sb.append("ACC_V[Z]"+fm(accValues[2])+BR);
+  }
+  if(accEnabled && magEnabled){
+    sb.append("P[X]"+fm(oriValues[1])+BR);
+    sb.append("R[Y]"+fm(oriValues[2])+BR);
+    sb.append("A[Z]"+fm(oriValues[0])+BR+BR);
+  }
+}
+
+public void onAccuracyChanged(Sensor sensor, int accuracy){
+}
+
+private String fm(float value){
+  return (value <= 0)?""+value:"+"+values;
+}
+
+}
 
 
 
