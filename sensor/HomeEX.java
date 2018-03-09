e6b5caf4dde0ccbd8b79cb67398df156bd2148b4
takagotch <dyaccb@gmail.com>
2018-03-09 20:04:00 +0900

    android:duration=@android:integer/config_mediumAnimTime

--- sensor/HomeEX.java
+++ sensor/HomeEX.java
@@ -13,6 +13,19 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import java.util.ArrayList;
 
+public class HomeEx extends AppCompatActivity
+  implements AdapterView.OnItemClickListener{
+  private final static int MP = ViewGroup.LayoutParams.MATCH_PARENT;
+
+}
 
 
