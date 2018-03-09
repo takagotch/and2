8f084592996c1a63e676af71e9ced4b6696396a0
takagotch <dyaccb@gmail.com>
2018-03-09 20:01:07 +0900

    android:duration=@android:integer/config_mediumAnimTime

--- sensor/HomeEX.java
+++ sensor/HomeEX.java
@@ -6,5 +6,13 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
 
 
