a5199c03574368aaaded908b2dc16661b3749a48
takagotch <dyaccb@gmail.com>
2018-03-09 19:58:24 +0900

    android:duration=@android:integer/config_mediumAnimTime

---
+++ sensor/HomeEX.java
@@ -0,0 +1,10 @@
+package net.tky.homeex;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
+import android.os.Bundle;
+
+
