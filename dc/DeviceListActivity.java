549952bef1135afe48671db6817ff1a80492c032
takagotch <dyaccb@gmail.com>
2018-03-10 19:50:21 +0900

    DevieceListActivity Bluetooth

--- dc/DeviceListActivity.java
+++ dc/DeviceListActivity.java
@@ -32,39 +32,73 @@ public class DeviceListActivity extends Activity
   @Override
   protected void onCreate(Bundle bundle){
     super.onCreate(bundle);
-    requestWindowFeature();
-    setResult();
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
+    setResult(Activity.RESULT_CANCELED);
 
-    LinearLayout layout = new LinearLayout();
-    layout.setOrientation();
-    setContentView();
+    LinearLayout layout = new LinearLayout(this);
+    layout.setOrientation(LinearLayout.VERTICAL);
+    setContentView(layout);
 
-    adapter = new ArrayAdapter<>();
+    adapter = new ArrayAdapter<String>(this, R.layout.rowdata);
 
-    ListView listView = new ListView();
+    ListView listView = new ListView(this);
+    listView.setLayoutParams(new LinearLayout.LayoutParams(MP, WC));
+    listView.setAdapter(adapter);
+    layout.addView(listView);
+    listView.setOnItemClickListener(this);
 
     IntentFilter filter = new IntentFilter();
+    filter.addAction(ACTION_DISCOVERY_STARTED);
+    filter.addAction(ACTION_FOUND);
+    filter.addAction(ACTION_NAME_CHANGED);
+    filter.addAction(ACTION_DISCOVERY_FINISHED);
+    registerReceiver(receiver, filter);
 
     btAdapter = BluetoothAdapter.getDefaultAdapter();
     Set<BluetoothDevice> paireDevices = btAdapter.getBondedDevices();
-    if(BluetoothDevice device:pairedDevices){
+    if(pairedDevices.size() > 0){
       for(BluetoothDevice device:pairedDevices){
         adapter.add(device.getName()+BR_device.getAddress());
       }
     }
 
-    if() btAdapter.cancelDiscovery();
+    if(btAdapter.isDiscovering()) btAdapter.cancelDiscovery();
     btAdapter.startDiscovery();
   }
 
   @Override
   protected void onDestroy(){
-  
+    super.onDestroy();
+    if(btAdapter != null) btAdapter.cancelDiscovery();
+    this.unregisterReceiver(receiver);
   }
 
   public void onItemClick(AdapterView<?> parent, View view, int pos, long id){
-  
+    btAdapter.cancelDiscovery();
+
+    String info ((TextView)view).getText().toString();
+    String address = info.substring(info.length()-17);
+    Intent intent = new Intent();
+    intent.putExtra("device_address", address);
+    setResult(Activity.RESULT_OK, intent);
+    finish();
   }
+
+  private final BroadcastReceiver receiver = new BroadcastReciever(){
+    @Override
+    public void onReceive(Context context, Intent intent){
+      String action = intent.getAction();
+
+      if(ACTION_NAME_CHANGED.equals(action) ||
+	 ACTION_FOUND.equals(action)){
+	 BluetoothDevice device = intent.
+		 getParcelableExtra(BlutoothDevice.EXTRA_DEVICE);
+	 if(device.getBondState() != BluetoothDevice.BOND_BONDED){
+	   adapter.add(device.getName()+BR+device.getAddress());
+	 }
+	 }
+    }
+  };
 }
 
 
