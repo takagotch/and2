718f0e2b0cac00fabf36a090582ab7f163861c30
takagotch <dyaccb@gmail.com>
2018-03-11 23:24:23 +0900

    DevieceListActivity Bluetooth

--- dc/BluetoothEx.java
+++ dc/BluetoothEx.java
@@ -13,7 +13,8 @@ public class BluetoothEx extends AppCompatActivity
   private BluetoothAdapter btAdapter;
   private ChatManager chatManager;
 
-  private EditText
+  private EditText edtSend;
+  private 
 }
 
 
