40f83b668990953d9ba1e9bec215ab032579b7ce
takagotch <dyaccb@gmail.com>
2018-03-11 23:22:35 +0900

    DevieceListActivity Bluetooth

---
+++ dc/BluetoothEx.java
@@ -0,0 +1,15 @@
+package net.tky.bluetoothex;
+import android.app.Activity;
+
+public class BluetoothEx extends AppCompatActivity
+  implements View.OnClickListener{
+  private final static int BR = System.getProperty("line.separator");
+  private final static int WC = ViewGroup.LayoutParams.WRAP_CONTENT;
+  private final static int MP = ViewGroup.LayoutParams.MATCH_PARENT;
+
+  private static final int RQ_CONNECT_DEVICE = 1;
+  private static 
+}
+
+
+
