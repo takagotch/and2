243d8085f97f2b37d5e23f7f943991981365daa4
takagotch <dyaccb@gmail.com>
2018-03-13 04:32:50 +0900

    <action android:name=net.tky.broadcastreceiver.VIEW />

---
+++ activity/FileProvider.java
@@ -0,0 +1,10 @@
+package net.tky.fileproviderex;
+import android.content.ContentProvider;
+import android.content.ContentValue;
+import android.
+
+public class FileProvider extends ContentProvider{
+
+}
+
+
