1b46ca867f2ed17957a890b673af0a301e4c4239
takagotch <dyaccb@gmail.com>
2018-03-13 04:13:26 +0900

    <action android:name=net.tky.broadcastreceiver.VIEW />

--- activity/MyService.java
+++ activity/MyService.java
@@ -53,22 +53,29 @@ public class MyService extends Service{
     builder.setContentText(text);
     builder.setSmallIcon(R.mipmap.ic_launcher);
 
-    Intent intent = new Intent();
-    intent.setComponent();
-    intent.removeCategory();
-    intent.addCategory();
-    intent.setFlags();
-    builder.setContentIntent();
+    Intent intent = new Intent(Intent.ACTION_MAIN);
+    intent.setComponent("net.tky.serviceex",
+		    "net.tky.serviceex.ServiceEx");
+    intent.removeCategory(Intent.CATEGORY_DEFAULT);
+    intent.addCategory(Intent.CATEGORY_LAUNCHER);
+    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    builder.setContentIntent(PendingIntent.getActivity(context, 0,
+			    intent, PendingIntent.FLAG_CANCEL_CURRENT));
 
-    NotificationManager nm = ();
-    nm.cancel();
-    nm.notify();
+    NotificationManager nm = (NotificationManager)
+	    context.getSystemService(Context.NOTIFICATION_SERVICE);
+    nm.cancel(1);
+    nm.notify(1, builder.build());
   }
 
-  private static void toast();
+  private static void toast(Context context, String text){
+    Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+  }
 
   private final IMyService.Stub IMyServiceBinder = new IMyService.Stub(){
-  
+    public void setMessage(String msg) throws RemoteException{
+      message = msg;
+    }
   };
 }
 
