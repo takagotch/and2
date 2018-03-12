7ea342bf981b0012e97053ca5e1d21c11660c372
takagotch <dyaccb@gmail.com>
2018-03-12 22:33:45 +0900

    LinearLayout layout = new LinearLayout(this);

---
+++ activity/MyActivity.java
@@ -0,0 +1,17 @@
+package net.tky.myactivityex;
+import android.app.Activity;
+import android.content.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+public class MyActivity extends Activity
+  implements View.OnClickListener{
+
+}
+
+
