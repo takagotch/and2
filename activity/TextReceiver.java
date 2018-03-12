f20225df540d8ab9c2999741da9f32bd67ca3994
takagotch <dyaccb@gmail.com>
2018-03-13 00:01:42 +0900

    <uses-permission android:name=android.permission.CALL_PHONE />

--- activity/TextReceiver.java
+++ activity/TextReceiver.java
@@ -1,8 +1,24 @@
 package net.tky.broadcastreceiverex;
-import
-import
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Colors;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class BroadcastReceiverEx extends Activity
   implement View.OnClickListener{
-  
+  private final static int WC = ViewGroup.LayoutParams.WRAP_CONTENT;
+
+  @Override
+  public void onCreate(Bundle bundle){
+    super.onCreate(bundle); 
+    requestWindowFeature();
   }
+}
+
+
+
