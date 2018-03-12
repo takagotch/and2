aeec16454c8de3b7337a02d16423207441303a37
takagotch <dyaccb@gmail.com>
2018-03-12 21:56:45 +0900

    implements View.OnClickListener{} private final static String TAG_WEB = web;

--- activity/ActivityEx.java
+++ activity/ActivityEx.java
@@ -1,12 +1,24 @@
 package net.tky.activityex;
-import
-import
-import
-import
+import android.app.Activity;
+import android.content.ComponetName;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class ActivityEx extends Activity
   implements View.OnClickListener{
-
+  private final static int WC = ViewGroup.LayoutParams.WRAP_CONTENT;
+  private final static String TAG_WEB = "web";
+  private final static String TAG_MAP = "map";
+  private final static String TAG_CALL = "call";
+  private final static String TAG_DEAL = "dial";
+  private final static String TAG_SETUP = "setup";
 }
 
 
