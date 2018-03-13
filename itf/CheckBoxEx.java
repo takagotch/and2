93b3f719037ff948951440a1f435e853cd3437c8
takagotch <dyaccb@gmail.com>
2018-03-14 01:56:47 +0900

    TextView textView = new TextView(this);

--- itf/CheckBoxEx.java
+++ itf/CheckBoxEx.java
@@ -2,7 +2,13 @@ package net.tky.checkboxex;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
-
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Checkbox;
+import android.widget.LinearLayout;
 
 public class CheckBoxEx extends Activity
   implements View.OnClickListener{
