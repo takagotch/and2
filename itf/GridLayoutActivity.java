3c83d32f69b3d5cdceb9ca86dbd721f2b645051a
takagotch <dyaccb@gmail.com>
2018-03-14 23:12:02 +0900

    button.setLayoutParams(new TableRow.LayoutParams(WC, WC));

---
+++ itf/GridLayoutActivity.java
@@ -0,0 +1,9 @@
+package net.tky.layoutex;
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.GridLayout;
+
